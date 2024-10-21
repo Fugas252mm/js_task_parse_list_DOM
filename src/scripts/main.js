f1d1019806fa721da0f76b253ea2f2d64e4ee6f1
Mykyta Zaluzhnyi <nikitaznr@gmail.com>
2024-10-21 14:19:31 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const transformToNumber = (str) => {
+  return parseFloat(str.replace(/[$,]/g, ''));
+};
+const getEmployees = (list) => {
+  return list.map((employee) => {
+    return {
+      name: employee.textContent.trim(),
+      salary: transformToNumber(employee.getAttribute('data-salary')),
+    };
+  });
+};
+const SortList = (list) => {
+  return list.sort((a, b) => b.salary - a.salary);
+};
+
+const employeesArray = Array.from(document.querySelectorAll('ul li'));
+const employeesData = employeesArray.map((employee) => {
+  getEmployees(employee);
+});
+
+SortList(employeesData);
